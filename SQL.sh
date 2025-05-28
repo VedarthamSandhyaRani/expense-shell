31449044ab6844c5c61cff5e4265406e891e331f
VedarthamSandhyaRani <sandhyaranivedartham12@gmail.com>
2025-05-28 22:00:54 +0530

    KLM

---
+++ SQL.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+
+sudo dnf install mysql-server -y  &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "Validate server"
