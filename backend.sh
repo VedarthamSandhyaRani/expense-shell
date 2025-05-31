81deb2708fb299dcc4ec65ab4879e503b8efdd93
VedarthamSandhyaRani <sandhyaranivedartham12@gmail.com>
2025-05-31 17:05:40 +0530

    LKG

--- backend.sh
+++ backend.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -8,7 +7,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+set -e
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
